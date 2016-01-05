8a020a3d0ae5c2efaf3a7c10de210372026496f9
nbt11nsn <niklassjogren_@hotmail.com>
2016-01-05 16:04:57 +0100

    FotmatDateTimeProperty PipelineComponent
    first commit

---
+++ FormatDateTimeProperties.Component.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using BizTalkComponents.Utils;
+
+namespace BizTalkComponents.PipelineComponents.FormatDateTimeProperties
+{
+    public partial class FormatDateTimeProperties
+    {
+        public string Name { get { return "Format DateTime Property Component"; } }
+        public string Version { get { return "0.1"; } }
+        public string Description { get { return @"Replaces a context value with specified datetime pattern"; } }
+
+        public void GetClassID(out Guid classid)
+        {
+            classid = new Guid("77043a51-4fdb-422a-b66a-de1a53957568");
+        }
+
+        public void InitNew() { }
+
+        public IntPtr Icon
+        {
+            get
+            {
+                return IntPtr.Zero;
+            }
+        }
+
+        public IEnumerator Validate(object projectSystem)
+        {
+            return ValidationHelper.Validate(this, false).ToArray().GetEnumerator();
+        }
+
+        public bool Validate(out string errorMessage)
+        {
+            var errors = ValidationHelper.Validate(this, true).ToArray();
+
+            if (errors.Any())
+            {
+                errorMessage = string.Join(",", errors);
+
+                return false;
+            }
+
+            errorMessage = string.Empty;
+
+            return true;
+        }
+    }
+}
---
+++ FormatDateTimeProperties.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using BizTalkComponents.Utils;
+using Microsoft.BizTalk.Component.Interop;
+using Microsoft.BizTalk.Message.Interop;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace BizTalkComponents.PipelineComponents.FormatDateTimeProperties
+{
+    [ComponentCategory(CategoryTypes.CATID_PipelineComponent)]
+    [ComponentCategory(CategoryTypes.CATID_Receiver)]
+    [ComponentCategory(CategoryTypes.CATID_Any)]
+    [System.Runtime.InteropServices.Guid("35db2d42-f633-4c96-bea7-a86c7a0350e9")]
+
+    public partial class FormatDateTimeProperties : IBaseComponent,
+        Microsoft.BizTalk.Component.Interop.IComponent,
+        IComponentUI,
+        IPersistPropertyBag
+    {
+        public virtual void Load(IPropertyBag pb, int errlog)
+        {
+        }
+
+        public virtual void Save(IPropertyBag pb, bool fClearDirty,
+            bool fSaveAllProperties)
+        {
+        }
+
+        public IBaseMessage Execute(IPipelineContext pContext, IBaseMessage pInMsg)
+        {
+            var result = DateTime.Now.ToString("yyMM");
+            pInMsg.Context.Promote("ReceivedFileName","http://schemas.microsoft.com/BizTalk/2003/file-properties", result);
+
+            return pInMsg;
+        }
+
+    }
+}
